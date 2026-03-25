4a6bfb57833dc9bca7dff898c73816a2d60e7617
Baktjan <bner012209@gmail.com>
2026-03-25 22:05:05 +0500

    raily

--- dacha_project/bot/dachabot.py
+++ dacha_project/bot/dachabot.py
@@ -1,14 +1,22 @@
 # bot/bot.py
 import os
+import asyncio
 from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
 from aiogram.types import WebAppInfo, ReplyKeyboardMarkup, KeyboardButton
+from dotenv import load_dotenv
 
+# Загружаем токен
+load_dotenv('../.env')  # путь к .env
 TOKEN = os.environ.get("Telegram_token")
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+print("TOKEN =", TOKEN)  # проверка токена
 
-@dp.message(commands=["start"])
+
+# Правильный хэндлер для команды /start
+@dp.message(Command("start"))
 async def start(msg: types.Message):
     kb = ReplyKeyboardMarkup(
         keyboard=[
@@ -21,4 +29,9 @@ async def start(msg: types.Message):
         ],
         resize_keyboard=True
     )
-    await msg.answer("123", reply_markup=kb)
+    await msg.answer("Привет! Я работаю ✅", reply_markup=kb)
+
+
+# Запуск бота
+if __name__ == "__main__":
+    asyncio.run(dp.start_polling(bot))
